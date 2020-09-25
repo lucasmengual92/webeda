1bfd2f90f52a41cbe79745b073dc0c20c780b2f0
Lucas <37154049+lucasmengual92@users.noreply.github.com>
2020-09-25 13:13:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 mkdir -p ~/.streamlit/
-
 echo "\
 [general]\n\
 email = \"l.mengual92@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
